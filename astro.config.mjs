7507724d9322fba50d1056f4c48082cf9e45ccdf
Steve Wyman <cloudventiresonline@gmail.com>
2026-03-12 14:02:49 +0000

    Add/update sitemap integration for auto-generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://bath-plumbers.co.uk',
   integrations: [
+    sitemap(),
     tailwind(),
   ],
   output: 'static',
